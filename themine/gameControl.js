5ea4a48b6951b3f678d19edcab97826282322eef
tlstetzer <tlstetzer@gmail.com>
2022-04-06 10:10:29 -0700

    Remove class

--- themine/gameControl.js
+++ themine/gameControl.js
@@ -1,37 +1,45 @@
-/* eslint no-unused-vars: 0 */
-/* eslint no-undef: 0 */
 /* eslint-env es6 */
+/* eslint no-unused-vars: 0 */
 
-class Game {
-	constructor(stage, root, lib) {
-		this.stage = stage;
-		this.root = root;
-		this.lib = lib;
-		this.init();
-	}
+// global variables
+var gameStage, root, lib, miner;
+var elev, win, bank, minerWalk;
 
-	init() {
-		const game = this;
+function gameInit(stage, exportRoot, compLib) {
+	// adobe globals
+	gameStage = stage;
+	root = exportRoot; 
+	lib = compLib;
 
-		// movie clips
-		this.elev = this.root.elevator_mc;
-		this.win = this.root.elevatorWindow_mc;
-		this.bank = this.root.town_mc;
+	// call functions
+	initMovieClips();
+	eventHandlers();
+}
 
-		// bank
-		this.root.btnBankOpen.on('click', function() { game.bank.gotoAndPlay('openDoor'); });
-		this.root.btnBankClose.on('click', function() { game.bank.gotoAndPlay('closeDoor'); });
-		
-		// elevator
-		this.root.btnElevOpen.on('click', function() { game.elev.gotoAndPlay('openDoor'); });
-		this.root.btnElevClose.on('click', function() { game.elev.gotoAndPlay('closeDoor'); });
-		this.root.btnElevDown.on('click', function() {  
-			game.elev.gotoAndPlay('elevDown'); 
-			game.win.gotoAndPlay('windowDown'); 
-		});
-		this.root.btnElevUp.on('click', function() { 
-			game.elev.gotoAndPlay('elevUp'); 
-			game.win.gotoAndPlay('windowUp'); 
-		});
-	}
+function initMovieClips() {
+	// elevator
+	win = root.elevatorWindow_mc;
+	elev = root.elevator_mc;
+	elev.loc = 'town';
+	
+	// town
+	bank = root.town_mc;
+}
+
+function eventHandlers() {
+	// bank
+	root.btnBankOpen.on('click', function() { bank.gotoAndPlay('openDoor'); });
+	root.btnBankClose.on('click', function() { bank.gotoAndPlay('closeDoor'); });
+
+	// elevator
+	root.btnElevOpen.on('click', function() { elev.gotoAndPlay('openDoor'); });
+	root.btnElevClose.on('click', function() { elev.gotoAndPlay('closeDoor'); });
+	root.btnElevDown.on('click', function() {  
+		elev.gotoAndPlay('elevDown'); 
+		win.gotoAndPlay('windowDown'); 
+	});
+	root.btnElevUp.on('click', function() { 
+		elev.gotoAndPlay('elevUp'); 
+		win.gotoAndPlay('windowUp'); 
+	});
 }
