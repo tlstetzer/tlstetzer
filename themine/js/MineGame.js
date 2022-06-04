25fb1f326184345720fc14c9d33c4824cf26d6c8
tlstetzer <tlstetzer@gmail.com>
2022-06-04 11:33:26 -0700

    Version .11

--- themine/js/MineGame.js
+++ themine/js/MineGame.js
@@ -1,62 +1,120 @@
-/* global root, Piece */
+/* global root, Piece, createjs */
 /* eslint no-unused-vars: 0 */
 
 // global variables
 var board = [];
-var anim, gb, game;
+var anim, gb, game, miner;
 
 // JavaScript Document
 class MineGame {
 	constructor() {
+		miner = { bank: 1000, gold: 0, piece: 'p0000', dir: 'left', x: 0, y: 0 };
 		game = this;
 		anim = root.animation_mc;
 		gb = root.gameBoard_mc;
+		
 		this.eventHandlers();
 	}
 
 	eventHandlers() {
+		// buttons
 		$('#btnStart').on('click', function() { game.buildBoard(); });
-		$('#btnExit').on('click', function() { 
-			anim.gotoAndPlay('exitBank'); 
-		});
-		$('#btnDown').on('click', function() { 
-			anim.gotoAndPlay('elevatorDown'); 
-		});
-		$('#btnPickaxe').on('click', function() {
-			anim.miner_mc.gotoAndPlay('pickaxe'); 
-			anim.miner_mc.gotoAndPlay('raisePickaxe'); 
-		});
-		$('#btnJackhammer').on('click', function() { 
-			anim.miner_mc.gotoAndPlay('jackhammer'); 
-			anim.miner_mc.gotoAndPlay('drill'); 
-		});
-		$('#btnDynamite').on('click', function() { 
-			anim.gotoAndPlay('explosive'); 
-			anim.gotoAndPlay('plunge'); 
-		});
-		$('#btnPump').on('click', function() { 
-			anim.gotoAndPlay('pump'); 
-			anim.gotoAndPlay('pumpOn'); 
-		});
-		$('#btnBank').on('click', function() { 
-			anim.gotoAndPlay('depositGold'); 
-		});
-		$('#btnCaveIn').on('click', function() { 
-			anim.gotoAndPlay('caveIn'); 
-		});
-		$('#btnSpring').on('click', function() { 
-			anim.gotoAndPlay('spring'); 
-		});
-		gb.btnLevel_01.on('click', function() { game.elevatorDown('level1'); });
-		gb.btnLevel_02.on('click', function() { game.elevatorDown('level2'); });
-		gb.btnLevel_03.on('click', function() { game.elevatorDown('level3'); });
-		gb.btnLevel_04.on('click', function() { game.elevatorDown('level4'); });
-		gb.btnLevel_05.on('click', function() { game.elevatorDown('level5'); });
-		gb.btnLevel_06.on('click', function() { game.elevatorDown('level6'); });
-		gb.btnLevel_07.on('click', function() { game.elevatorDown('level7'); });
-		gb.btnLevel_08.on('click', function() { game.elevatorDown('level8'); });
-		gb.btnLevel_09.on('click', function() { game.elevatorDown('level9'); });
-		gb.btnLevel_10.on('click', function() { game.elevatorDown('level10'); });
+		
+		//movement
+		$('#btnLeft').on('click', function() { game.moveLeft(); });
+		$('#btnRight').on('click', function() { game.moveRight(); });
+		$('#btnUp').on('click', function() { game.moveUp(); });
+		$('#btnDown').on('click', function() { game.moveDown(); });
+		
+		// tools
+		$('#btnPickaxe').on('click', function() { anim.gotoAndPlay('pickaxe'); });
+		$('#btnJackhammer').on('click', function() { anim.gotoAndPlay('jackhammer'); });
+		$('#btnDynamite').on('click', function() { anim.gotoAndPlay('dynamite'); });
+		$('#btnPump').on('click', function() { anim.gotoAndPlay('pump'); });
+		
+		// level buttons
+		gb.btnLevel_1.on('click', function() { game.elevatorDown('level1'); miner.x = 339.5; miner.y = -139.35; miner.piece = 'p0131'; });
+		gb.btnLevel_2.on('click', function() { game.elevatorDown('level2'); miner.x = 339.5; miner.y = -111.35; miner.piece = 'p0231'; });
+		gb.btnLevel_3.on('click', function() { game.elevatorDown('level3'); miner.x = 339.5; miner.y = -83.35; miner.piece = 'p0331'; });
+		gb.btnLevel_4.on('click', function() { game.elevatorDown('level4'); miner.x = 339.5; miner.y = -54.85; miner.piece = 'p0431'; });
+		gb.btnLevel_5.on('click', function() { game.elevatorDown('level5'); miner.x = 339.5; miner.y = -27.6; miner.piece = 'p0531'; });
+		gb.btnLevel_6.on('click', function() { game.elevatorDown('level6'); miner.x = 339.5; miner.y = 1.15; miner.piece = 'p0631'; });
+		gb.btnLevel_7.on('click', function() { game.elevatorDown('level7'); miner.x = 339.5; miner.y = 28.65; miner.piece = 'p0731'; });
+		gb.btnLevel_8.on('click', function() { game.elevatorDown('level8'); miner.x = 339.5; miner.y = 56.65; miner.piece = 'p0831'; });
+		gb.btnLevel_9.on('click', function() { game.elevatorDown('level9'); miner.x = 339.5; miner.y = 84.65; miner.piece = 'p0931'; });
+		gb.btnLevel_10.on('click', function() { game.elevatorDown('level10'); miner.x = 339.5; miner.y = 108.9; miner.piece = 'p1031'; });
+	}
+	
+	moveLeft() {
+		var piece = board.find(p => p.getID() == miner.piece);
+		if(piece.getCol() == 1) { gb.info_text.text = 'Cannot move that direction!'; }
+		else {
+			var nCol = piece.getCol() - 1;
+			var nID = 'p' + game.padValue(piece.getRow()) + game.padValue(nCol);
+			var nPiece = board.find(p => p.getID() == miner.piece);
+			// has the piece already been dug
+			// is the piece an item of interest
+			// check tool
+			miner.piece = nID;
+			nPiece.setIsDug(true);
+			miner.x -= 28;
+			gb.getChildByName(nID).gotoAndStop('dug');
+			createjs.Tween.get(gb.gameMiner_mc).to({x: miner.x}, 1000);
+		}
+	}
+	
+	moveRight() {
+		var piece = board.find(p => p.getID() == miner.piece);
+		if(piece.getCol() == 30) { gb.info_text.text = 'Go to bank!'; }
+		else {
+			var nCol = piece.getCol() + 1;
+			var nID = 'p' + game.padValue(piece.getRow()) + game.padValue(nCol);
+			var nPiece = board.find(p => p.getID() == miner.piece);
+			// has the piece already been dug
+			// is the piece an item of interest
+			// check tool
+			miner.piece = nID;
+			nPiece.setIsDug(true);
+			miner.x += 28;
+			gb.getChildByName(nID).gotoAndStop('dug');
+			createjs.Tween.get(gb.gameMiner_mc).to({x: miner.x}, 1000);
+		}
+	}
+	
+	moveUp() {
+		var piece = board.find(p => p.getID() == miner.piece);
+		if(piece.getRow() == 10) { gb.info_text.text = 'Cannot move that direction!'; }
+		else {
+			var nRow = piece.getRow() - 1;
+			var nID = 'p' + game.padValue(nRow) + game.padValue(piece.getCol());
+			var nPiece = board.find(p => p.getID() == miner.piece);
+			// has the piece already been dug
+			// is the piece an item of interest
+			// check tool
+			miner.piece = nID;
+			nPiece.setIsDug(true);
+			miner.y -= 28;
+			gb.getChildByName(nID).gotoAndStop('dug');
+			createjs.Tween.get(gb.gameMiner_mc).to({y: miner.y}, 1000);
+		}
+	}
+	
+	moveDown() {
+		var piece = board.find(p => p.getID() == miner.piece);
+		if(piece.getRow() == 10) { gb.info_text.text = 'Cannot move that direction!'; }
+		else {
+			var nRow = piece.getRow() + 1;
+			var nID = 'p' + game.padValue(nRow) + game.padValue(piece.getCol());
+			var nPiece = board.find(p => p.getID() == miner.piece);
+			// has the piece already been dug
+			// is the piece an item of interest
+			// check tool
+			miner.piece = nID;
+			nPiece.setIsDug(true);
+			miner.y += 28;
+			gb.getChildByName(nID).gotoAndStop('dug');
+			createjs.Tween.get(gb.gameMiner_mc).to({y: miner.y}, 1000);
+		}
 	}
 	
 	elevatorDown(level) {
@@ -66,26 +124,18 @@ class MineGame {
 	
 	buildBoard() {
 		board = [];
-		var pieceType = [ 
-			'rock', 'rock', 'rock', 'rock', 'rock', 'rock', 'rock', 'rock', 'rock', 'rock', 
-			'gold', 'gold', 
-			'hard',  
-			'water',
-			'caveIn'
-		];
+		gb.info_text.text = 'Please wait while mine is scanned!';
 		
 		for(var row=1; row<11; row++) {
-			for(var col=1; col<31; col++) {
-				var rnd = game.random(15);
-				var piece = new Piece(row, col, pieceType[rnd], this);
+			for(var col=1; col<32; col++) {
+				var piece = new Piece(row, col, 'rock', this);
 				var pid = piece.getID();
 
 				board.push(piece);
-				if(pieceType[rnd] == 'rock') { gb.getChildByName(pid).gotoAndStop('rock'); }
-				else { gb.getChildByName(pid).gotoAndStop('interest'); }
+				if(col < 31) { gb.getChildByName(pid).gotoAndStop('rock'); }
 			}
 		}
-		anim.gotoAndPlay('exitBank'); 
+		game.placeGold(150); 
 	}
 
 	random(seed) {
@@ -96,6 +146,25 @@ class MineGame {
 	padValue(value) {
 		var pVal = '00' + String(value);
 		return pVal.slice(-2);
+	}
+	
+	placeGold(seed) {
+		if(seed > 0) {
+			var row = game.random(10);
+			var col = game.random(30);
+			var pid = 'p' + game.padValue(row) + game.padValue(col);
+			var piece = board.find(p => p.getID() == pid);
+			if(piece.getType() == 'rock') {
+				piece.setType('interest');
+				gb.getChildByName(pid).gotoAndStop('interest');
+				seed--;
+			}
+			game.placeGold(seed);
+		} else {
+			var info = gb.info_text;
+			gb.info_text.text = 'Select your Tunnel!';
+			anim.gotoAndPlay('exitBank');
+		}
 	}
 
 }
--- themine/js/Piece.js
+++ themine/js/Piece.js
@@ -6,13 +6,16 @@ class Piece {
 		this.ID = 'p' + mineGame.padValue(row) + mineGame.padValue(col);
 		this.type = type;
 		this.isDug = false;
+		this.row = row;
+		this.col = col;
 	}
 	
 	getID() { return this.ID; }
 	getType() { return this.type; }
 	getIsDug() { return this.isDug; }
-	
-	setID(value) { this.ID = value; }
+	getRow() { return this.row; }
+	getCol() { return this.col; }
+
 	setType(value) { this.type = value; }
 	setIsDug(value) { this.getIsDug = value; }
 }
